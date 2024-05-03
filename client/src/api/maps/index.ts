7bd3d44079e5d6e9e2e14b7d1d7642388ed9be19
vlad1013374 <80113777+vlad1013374@users.noreply.github.com>
2024-05-03 17:30:11 +0300

    delete unused api folder

--- client/src/api/maps/index.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const $mapApi = axios.create({
-    baseURL: "https://nominatim.openstreetmap.org/",
-    headers: {
-        "Access-Control-Allow-Origin": "https://o2cj2q.csb.app"
-      }
-})
